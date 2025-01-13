cbfa7a424e516378608013381f8dc3db86d5d8d7
nblhmrh <nabilahhumairah@gmail.com>
2025-01-13 14:02:56 +0800

    aaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1051,7 +1051,9 @@
                                 <h2 class="text-xl font-semibold text-black dark:text-white">Laravel News</h2>
 
                                 <p class="mt-4 text-sm/relaxed">
-                                    EXO
+                                    Laravel News is a community driven portal and newsletter aggregating all of the
+                                    latest and most important news in the Laravel ecosystem, including new package
+                                    releases and tutorials.
                                 </p>
                             </div>
 
